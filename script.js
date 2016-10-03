832fcf76d019e4f58901b121277881b31de8943f
regiscodales <saregis@hotmail.com>
2016-10-03 10:11:03 +0200

    jquery

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+$(document).ready(function() {
+	$("ul:visible").hide();
+
+
+
+	$(".bouton1").click(function() {
+		$(".parcours ul").toggle("slow");
+	});
+	$(".bouton2").click(function() {
+		$(".diplome ul").toggle("slow");
+	});
+	$(".bouton3").click(function() {
+		$(".passion ul").toggle("slow");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
